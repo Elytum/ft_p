c4d522063fc5f8d63f8fb0c3fe2a3867ccad6525
Arthur CHAZAL <achazal@e2r2p21.42.fr>
2015-05-28 09:55:28 +0200

    Push

---
+++ common/inputs/srcs/ft_cursor.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cursor.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achazal <achazal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/22 08:08:45 by achazal           #+#    #+#             */
+/*   Updated: 2015/01/22 08:23:47 by achazal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <inputs.h>
+
+void		ft_update_input(t_env *e)
+{
+	int len;
+
+	tputs(e->str, 1, ft_putc);
+	len = e->max - e->index;
+	while (len--)
+		tputs(tgetstr("le", (char **)(&e->p->buf)), 1, ft_putc);
+}
+
+void		ft_endline(t_env *e)
+{
+	while (e->index < e->max)
+		ft_goright(e);
+	tputs("\n", 1, ft_putc);
+}
---
+++ common/libft/ft_lstadd_end.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_end.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsirigna <lsirigna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/18 16:05:20 by lsirigna          #+#    #+#             */
+/*   Updated: 2015/02/18 16:05:25 by lsirigna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_end(t_list **alst, t_list *new)
+{
+	t_list		*tmp;
+
+	tmp = *alst;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+}
